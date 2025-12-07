0cd395468ecda73ed719237f62ad308408ed1e22
Ihor <dwarionas@gmail.com>
2025-12-08 00:37:27 +0100

    nodes

--- bot/default_products.ts
+++ bot/default_products.ts
@@ -1,25 +1,25 @@
 import type { Product } from "./types/types.js";
 
 const default_products: Product[] = [
-  { "name": "Куряча грудка", "type": "gram", "value": 31 },
-  { "name": "Індичка", "type": "gram", "value": 29 },
-  { "name": "Яловичина", "type": "gram", "value": 26 },
-  { "name": "Свинина", "type": "gram", "value": 25 },
-  { "name": "Лосось", "type": "gram", "value": 20 },
-  { "name": "Тунець", "type": "gram", "value": 25 },
-  { "name": "Оселедець", "type": "gram", "value": 20 },
-  { "name": "Яйце куряче", "type": "stuck", "value": 6 },
-  { "name": "Сир кисломолочний", "type": "gram", "value": 17 },
-  { "name": "Сири твердi (пармезан)", "type": "gram", "value": 30 },
-  { "name": "Сири напівтверді (чеддер, гауда)", "type": "gram", "value": 25 },
-  { "name": "Грецький йогурт", "type": "gram", "value": 10 },
-  { "name": "Молоко", "type": "gram", "value": 3.3 },
-  { "name": "Горох, нут, квасоля (варені)", "type": "gram", "value": 8 },
-  { "name": "Сочевиця (варена)", "type": "gram", "value": 9 },
-  { "name": "Тофу", "type": "gram", "value": 8 },
-  { "name": "Протеїн", "type": "stuck", "value": 22 },
-  { "name": "Арахіс", "type": "gram", "value": 25 },
-  { "name": "Мигдаль", "type": "gram", "value": 21 }
+  { "name": "Куряча грудка", "type": "gram", "value": 31, "amountToday": 0 },
+  { "name": "Індичка", "type": "gram", "value": 29, "amountToday": 0 },
+  { "name": "Яловичина", "type": "gram", "value": 26, "amountToday": 0 },
+  { "name": "Свинина", "type": "gram", "value": 25, "amountToday": 0 },
+  { "name": "Лосось", "type": "gram", "value": 20, "amountToday": 0 },
+  { "name": "Тунець", "type": "gram", "value": 25, "amountToday": 0 },
+  { "name": "Оселедець", "type": "gram", "value": 20, "amountToday": 0 },
+  { "name": "Яйце куряче", "type": "stuck", "value": 6, "amountToday": 0 },
+  { "name": "Сир кисломолочний", "type": "gram", "value": 17, "amountToday": 0 },
+  { "name": "Сири твердi (пармезан)", "type": "gram", "value": 30, "amountToday": 0 },
+  { "name": "Сири напівтверді (чеддер, гауда)", "type": "gram", "value": 25, "amountToday": 0 },
+  { "name": "Грецький йогурт", "type": "gram", "value": 10, "amountToday": 0 },
+  { "name": "Молоко", "type": "gram", "value": 3.3, "amountToday": 0 },
+  { "name": "Горох, нут, квасоля (варені)", "type": "gram", "value": 8, "amountToday": 0 },
+  { "name": "Сочевиця (варена)", "type": "gram", "value": 9, "amountToday": 0 },
+  { "name": "Тофу", "type": "gram", "value": 8, "amountToday": 0 },
+  { "name": "Протеїн", "type": "stuck", "value": 22, "amountToday": 0 },
+  { "name": "Арахіс", "type": "gram", "value": 25, "amountToday": 0 },
+  { "name": "Мигдаль", "type": "gram", "value": 21, "amountToday": 0 }
 ]
 
 
